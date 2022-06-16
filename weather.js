766d886132558d47a67555a5ed1ab2fc24ad5957
brs14shn <brsshn292929@gmail.com>
2022-06-16 20:31:33 +0300

    first commit

---
+++ weather.js
@@ -0,0 +1,11 @@
+const form=document.querySelector(".top-banner form");
+
+const input=document.querySelector(".top-banner input");
+
+const msg=document.querySelector("span.msg");
+
+const list=document.querySelector(".ajax-section .cities")
+
+
+localStorage.setItem("apiKey",EncryptStringAES("346b0899c9b7d0f52312e560ac10f74e"));
+
